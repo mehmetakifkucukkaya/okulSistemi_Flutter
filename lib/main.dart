8190038b3c640dd7274965df3cf33c15bbb349eb
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2024-03-11 01:38:52 +0300

    Created Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Okul Sistemi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Okul Sistemi'),
@@ -31,39 +31,89 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    var ekranBilgisi = MediaQuery.of(context);
+
+    final ekranYuksekligi = ekranBilgisi.size.height;
+    final ekranGenisligi = ekranBilgisi.size.width;
+
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
+      backgroundColor: Colors.white,
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: ekranYuksekligi / 70),
+                child: SizedBox(
+                  width: ekranGenisligi / 2,
+                  child: Image.asset("assets/Icon.png"),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(ekranGenisligi / 40),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Kullanıcı Adı",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    )),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(ekranGenisligi / 40),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    hintText: "Şifre",
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    )),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(ekranYuksekligi / 50),
+                child: SizedBox(
+                  height: ekranYuksekligi / 9,
+                  width: ekranGenisligi / 1.4,
+                  child: TextButton(
+                    onPressed: () {
+                      print("Giriş Yapıldı");
+                    },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(6.0),
+                        ),
+                      ),
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.grey),
+                    ),
+                    child: Text(
+                      "Giriş Yap",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: ekranGenisligi / 26),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
